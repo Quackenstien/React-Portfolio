8b3351354a66b431f38736481472a83b723912fe
Quackenstien <donaldhawkins422@gmail.com>
2020-07-13 19:58:12 -0700

    Updates

---
+++ precache-manifest.3e195c6f079bf37d421a923b0050967a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda7d7a823779901899ab35895f74d00",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "1db3dae707242e7ef968",
+    "url": "/React-Portfolio/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "b0166e3644777b44b339",
+    "url": "/React-Portfolio/static/js/2.ecb1775f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.ecb1775f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db3dae707242e7ef968",
+    "url": "/React-Portfolio/static/js/main.5f4c916b.chunk.js"
+  },
+  {
+    "revision": "d1edeb6cf76e46605df7",
+    "url": "/React-Portfolio/static/js/runtime-main.7d26421e.js"
+  },
+  {
+    "revision": "360e478b704e3144f6a2ecaf14a75d8c",
+    "url": "/React-Portfolio/static/media/noats.360e478b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/.github.io/React-Portfolio/precache-manifest.93165bd1c1f4511913387318b1524f8a.js"
+  "/React-Portfolio/precache-manifest.3e195c6f079bf37d421a923b0050967a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.github.io/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
